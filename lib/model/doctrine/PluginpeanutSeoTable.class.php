872e474d27c2ec15919fa8fc449ccc2cce44881c
Alexandre pocky Balmes <albalmes@gmail.com>
2010-08-02 22:53:23 +0200

    Ajout du module

--- lib/model/doctrine/PluginpeanutSeoTable.class.php
+++ lib/model/doctrine/PluginpeanutSeoTable.class.php
@@ -7,5 +7,15 @@ abstract class PluginpeanutSeoTable extends Doctrine_Table
   {
     return Doctrine_Core::getTable('peanutSeo');
   }
+  
+  
+  public function getSitemap($model, $status = 'publish')
+  {
+    $p = Doctrine_Query::create()
+      ->from($model . ' p')
+      ->where('p.status = ?', $status)
+      ->orderBy('updated_at');
     
+    return $p->execute(array(), Doctrine_Core::HYDRATE_ARRAY);
+  }
 }
